cf8b1f3c14a6d1563eecb85267204ffd6d2a8091
Lupus590 <lupussolitarius590@gmail.com>
2023-12-27 16:31:47 +0000

    Mod info file
    with the original images in their higher quality size.

--- src/mod.cpp
+++ src/mod.cpp
@@ -1,6 +1,7 @@
 name = "34th Para-Recon Company";
-picture = "34th_PRC.paa";
-overview = "Brimstone";
-action = "";
-logo = "34th_PRC.paa";
-logoOver = "";
+picture = "34thPRC.paa";
+overview = "The aux mod for the 34th Para-Recon ""Brimstone"" Company.<br/>You probably only need this if you are in our unit.";
+logo = "34thPRC.paa";
+logoOver = "34thPRC_LowVis.paa";
+actionName = "GitHub";
+action = "https://github.com/34th-Para-Recon-Brimstone-Company/34th-PRC-Aux-Mod";
