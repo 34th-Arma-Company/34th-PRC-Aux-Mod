a5d074aada1c029a1774c5ac7fabed187e728208
Lupus590 <lupussolitarius590@gmail.com>
2023-12-30 22:07:10 +0000

    Custom beret (#10)

---
+++ src/Addons/34thPRC_Armour_Standard/data/beret/config.hpp
@@ -0,0 +1,22 @@
+class H_Beret_02; // Vanilla
+class 34thPRC_Armour_Standard_Headgear_Beret_01: H_Beret_02
+{
+	displayName="[34th] Beret 01";
+	author="Lupus590";
+	scope=2;
+	scopeArsenal=2;
+	scopeCurator=2;
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_Armour_Standard\data\beret\beret_01.paa",
+	};
+};
+class 34thPRC_Armour_Standard_Headgear_Beret_02: 34thPRC_Armour_Standard_Headgear_Beret_01
+{
+	displayName="[34th] Beret 02";
+	author="Lupus590";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_Armour_Standard\data\beret\beret_02.paa",
+	};
+};
---
+++ src/Addons/34thPRC_Armour_Standard/data/config.hpp
@@ -0,0 +1,1 @@
+#include "beret\config.hpp"
