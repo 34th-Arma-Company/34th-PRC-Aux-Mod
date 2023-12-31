151cba00ca7bdbd72da74b7031c5f89e49695aef
DaCapa <dr.34dc@gmail.com>
2023-12-31 17:33:41 -0500

    WIP ODST Armour and Helm
    Helmet is good.
    Visor is messed up, needs to be the SILVER one.
    Vest is getting worked.
    Inheritance needs to be changed from someone's custom armour to a new "base"

---
+++ src/Addons/34thPRC_Armour_Standard/data/odst/config_weapons.hpp
@@ -0,0 +1,31 @@
+class MA_M56SR_Helmet_Binns; // MA_Armor
+class 34thPRC_Armour_Standard_Headgear_ODST_Urban : MA_M56SR_Helmet_Binns
+{
+    displayName="[34th] M56SR";
+    author= "Cappy";
+    hiddenSelectionsTextures[]=
+    {
+        "34thPRC_Armour_Standard\data\odst\34_odst_helmet",
+        "MA_Armor\data\Helmets\HR_ODST\HR_ODST_Visor_Silver_co.paa"
+    };
+};
+
+class MA_M56R_Vest_Binns; // MA_Armor
+class 34thPRC_Armour_Standard_Vest_ODST_Urban: MA_M56R_Vest_Binns
+{
+    displayName="[34th] M56R Combat Armour";
+    author= "Cappy"
+    hiddenSelectionsTextures[]=
+    {
+            "MA_Armor\data\Vests\H3_ODST\H3_Upper_Armor_co.paa",       //camo1 (H3 Upper Armor)
+            "MA_Armor\data\Vests\H3_ODST\H3_Lower_Armor_co.paa",       //camo2 (H3 Lower Armor)
+            "MA_Armor\data\Vests\H3_ODST\Addons\Thigh_Pouch_co.paa",//camo3 (Thigh Pouch)
+            "34thPRC_Armour_Standard\data\odst\34_odst_upper",    //camo4 (Reach Upper Armor)
+            "34thPRC_Armour_Standard\data\odst\34_odst_lower",    //camo5 (Reach Lower Armor)
+            "MA_Armor\data\Vests\Reach_ODST\Canisters_co.paa",      //camo6 (Canisters)
+            "MA_Armor\data\Vests\Reach_ODST\Forearm_Vents_co.paa",   //camo7 (Forearm Vents)
+            "MA_Armor\data\Dev_Gear\Binns\Sniper_Pauldron_Binns.paa", //camo8 (Sniper Shoulders)
+            "MA_Armor\data\Vests\Reach_ODST\Addons\CQB_Shoulders\CQB_co.paa",  //camo9 (CQB Shoulders)
+            "MA_Armor\data\Vests\Reach_ODST\Addons\Reach_Shoulder_Radio\Shoulder_Radio_ODST_co.paa" //camo10 (Shoulder Radio)
+    };
+};
