6b43cbea94be3ac2a1d765f477b921aa1c3369e0
DaCapa <dr.34dc@gmail.com>
2023-12-31 18:04:32 -0500

    ODST Fixes
    Inheritance Fixed
    Vest Fixed
    Visor Fixed

--- src/Addons/34thPRC_Armour_Standard/data/odst/config_weapons.hpp
+++ src/Addons/34thPRC_Armour_Standard/data/odst/config_weapons.hpp
@@ -1,27 +1,27 @@
-class MA_M56SR_Helmet_Binns; // MA_Armor
-class 34thPRC_Armour_Standard_Headgear_ODST_Urban : MA_M56SR_Helmet_Binns
+class MA_M56SR_Helmet_White_Visor; // MA_Armor
+class 34thPRC_Armour_Standard_Headgear_ODST_Urban : MA_M56SR_Helmet_White_Visor
 {
     displayName="[34th] M56SR";
     author= "Cappy";
     hiddenSelectionsTextures[]=
     {
         "34thPRC_Armour_Standard\data\odst\34_odst_helmet",
-        "MA_Armor\data\Helmets\HR_ODST\HR_ODST_Visor_Silver_co.paa"
+        "MA_Armor\data\Helmets\HR_ODST\HR_ODST_Visor_White_co.paa"
     };
 };
 
-class MA_M56R_Vest_Binns; // MA_Armor
-class 34thPRC_Armour_Standard_Vest_ODST_Urban: MA_M56R_Vest_Binns
+class MA_M56R_Vest; // MA_Armor
+class 34thPRC_Armour_Standard_Vest_ODST_Urban: MA_M56R_Vest
 {
     displayName="[34th] M56R Combat Armour";
     author= "Cappy"
-    hiddenSelectionsTextures[]=
+	hiddenSelectionsTextures[]=
     {
             "MA_Armor\data\Vests\H3_ODST\H3_Upper_Armor_co.paa",       //camo1 (H3 Upper Armor)
             "MA_Armor\data\Vests\H3_ODST\H3_Lower_Armor_co.paa",       //camo2 (H3 Lower Armor)
             "MA_Armor\data\Vests\H3_ODST\Addons\Thigh_Pouch_co.paa",//camo3 (Thigh Pouch)
-            "34thPRC_Armour_Standard\data\odst\34_odst_upper",    //camo4 (Reach Upper Armor)
-            "34thPRC_Armour_Standard\data\odst\34_odst_lower",    //camo5 (Reach Lower Armor)
+            "34thPRC_Armour_Standard\data\odst\34_odst_upper.paa",    //camo4 (Reach Upper Armor)
+            "MA_Armor\data\Vests\Reach_ODST\Reach_Armor_Lower_co.paa",    //camo5 (Reach Lower Armor)
             "MA_Armor\data\Vests\Reach_ODST\Canisters_co.paa",      //camo6 (Canisters)
             "MA_Armor\data\Vests\Reach_ODST\Forearm_Vents_co.paa",   //camo7 (Forearm Vents)
             "MA_Armor\data\Dev_Gear\Binns\Sniper_Pauldron_Binns.paa", //camo8 (Sniper Shoulders)
