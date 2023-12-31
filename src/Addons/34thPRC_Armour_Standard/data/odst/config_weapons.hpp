60dba0b8e8bc3d3fdc786f21954230abde080642
Lupus590 <lupussolitarius590@gmail.com>
2023-12-31 23:10:29 +0000

    Binns sniper shoulder -> generic sniper shoulder

--- src/Addons/34thPRC_Armour_Standard/data/odst/config_weapons.hpp
+++ src/Addons/34thPRC_Armour_Standard/data/odst/config_weapons.hpp
@@ -24,7 +24,7 @@ class 34thPRC_Armour_Standard_Vest_ODST_Urban: MA_M56R_Vest
             "MA_Armor\data\Vests\Reach_ODST\Reach_Armor_Lower_co.paa",    //camo5 (Reach Lower Armor)
             "MA_Armor\data\Vests\Reach_ODST\Canisters_co.paa",      //camo6 (Canisters)
             "MA_Armor\data\Vests\Reach_ODST\Forearm_Vents_co.paa",   //camo7 (Forearm Vents)
-            "MA_Armor\data\Dev_Gear\Binns\Sniper_Pauldron_Binns.paa", //camo8 (Sniper Shoulders)
+            "MA_Armor\data\Vests\Reach_ODST\Addons\Sniper_Shoulders\Sniper_co.paa", //camo8 (Sniper Shoulders)
             "MA_Armor\data\Vests\Reach_ODST\Addons\CQB_Shoulders\CQB_co.paa",  //camo9 (CQB Shoulders)
             "MA_Armor\data\Vests\Reach_ODST\Addons\Reach_Shoulder_Radio\Shoulder_Radio_ODST_co.paa" //camo10 (Shoulder Radio)
     };
