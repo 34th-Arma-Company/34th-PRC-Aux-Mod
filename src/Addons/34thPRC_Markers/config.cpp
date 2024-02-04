0cdd39404415fb127625ee49c0fe5aafec9e5632
Lupus590 <lupussolitarius590@gmail.com>
2024-02-04 16:55:41 +0000

    Map markers

---
+++ src/Addons/34thPRC_Markers/config.cpp
@@ -0,0 +1,58 @@
+class CfgPatches
+{
+    class 34thPRC_Markers
+    {
+		author="34th PRC Modding Team";
+		units[]={};
+    };
+};
+class CfgMarkerClasses
+{
+    class 34thPRC_Map_Markers
+	{
+		displayName="34th PRC Markers";
+    };
+};
+class CfgMarkers
+{
+    class 34thPRC_Marker1
+    {
+        name="[34th] 34th PRC 1";
+        icon="34thPRC_Markers\data\34ParaReconVer1.paa";
+        color[]={0,0,0,1};
+        size=40;
+        shadow = 0;
+        scope = 2;
+        markerClass = "34thPRC_Map_Markers";
+    };
+    class 34thPRC_Marker2
+    {
+        name="[34th] 34th PRC 2";
+        icon="34thPRC_Markers\data\34ParaReconVer2.paa";
+        color[]={0,0,0,1};
+        size=40;
+        shadow = 0;
+        scope = 2;
+        markerClass = "34thPRC_Map_Markers";
+    };
+    class 34thPRC_Marker3
+    {
+        name="[34th] 34th PRC 3";
+        icon="34thPRC_Markers\data\34ParaReconVer3.paa";
+        color[]={0,0,0,1};
+        size=40;
+        shadow = 0;
+        scope = 2;
+        markerClass = "34thPRC_Map_Markers";
+    };
+    class 34thPRC_Marker4
+    {
+        name="[34th] 34th PRC 4";
+        icon="34thPRC_Markers\data\34ParaReconVer4.paa";
+        color[]={0,0,0,1};
+        size=40;
+        shadow = 0;
+        scope = 2;
+        markerClass = "34thPRC_Map_Markers";
+    };
+};
