866da7defb3bee5029a50dc97f4498b10ce46f9c
Lupus590 <lupussolitarius590@gmail.com>
2024-02-04 18:32:54 +0000

    Armour

--- src/Addons/34thPRC_Armour_Standard/data/infantry/config_vehicles.hpp
+++ src/Addons/34thPRC_Armour_Standard/data/infantry/config_vehicles.hpp
@@ -11,3 +11,42 @@ class 34thPRC_Armour_Standard_Trooper_Rifleman_Urban : HaloInf_Marine_GRFS_F
 		"34thPRC_Armour_Standard\data\infantry\shoulders_co.paa"
 	};
 }
+
+class 34thPRC_Armour_Standard_Trooper_Rifleman_Urban2 : HaloInf_Marine_GRFS_F
+{
+	displayName="[34th] Uniform2";
+	author="Cappy";
+	uniformClass="34thPRC_Armour_Standard_Uniform_Rifleman_Urban2";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_Armour_Standard\data\infantry\shirt_co.paa",
+		"34thPRC_Armour_Standard\data\infantry\legs_co.paa",
+		"34thPRC_Armour_Standard\data\infantry\shoulders2_co.paa"
+	};
+}
+
+class 34thPRC_Armour_Standard_Trooper_Rifleman_Urban3 : HaloInf_Marine_GRFS_F
+{
+	displayName="[34th] Uniform3";
+	author="Cappy";
+	uniformClass="34thPRC_Armour_Standard_Uniform_Rifleman_Urban3";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_Armour_Standard\data\infantry\shirt_co.paa",
+		"34thPRC_Armour_Standard\data\infantry\legs_co.paa",
+		"34thPRC_Armour_Standard\data\infantry\shoulders3_co.paa"
+	};
+}
+
+class 34thPRC_Armour_Standard_Trooper_Rifleman_Urban4 : HaloInf_Marine_GRFS_F
+{
+	displayName="[34th] Uniform4";
+	author="Cappy";
+	uniformClass="34thPRC_Armour_Standard_Uniform_Rifleman_Urban4";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_Armour_Standard\data\infantry\shirt_co.paa",
+		"34thPRC_Armour_Standard\data\infantry\legs_co.paa",
+		"34thPRC_Armour_Standard\data\infantry\shoulders4_co.paa"
+	};
+}
--- src/Addons/34thPRC_Armour_Standard/data/infantry/config_weapons.hpp
+++ src/Addons/34thPRC_Armour_Standard/data/infantry/config_weapons.hpp
@@ -35,3 +35,69 @@ class 34thPRC_Armour_Standard_Uniform_Rifleman_Urban : haloinfmar_U_GRFS_uniform
 		uniformClass="34thPRC_Armour_Standard_Trooper_Rifleman_Urban";
 	}
 }
+
+class 34thPRC_Armour_Standard_Uniform_Rifleman_Urban2 : haloinfmar_U_GRFS_uniform
+{
+	displayName="[34th] Uniform2";
+	author="Cappy";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_Armour_Standard\data\infantry\shirt_co.paa",
+		"34thPRC_Armour_Standard\data\infantry\legs_co.paa",
+		"34thPRC_Armour_Standard\data\infantry\shoulders2_co.paa"
+	};
+	class ItemInfo : ItemInfo
+	{
+		hiddenSelectionsTextures[]=
+		{
+			"34thPRC_Armour_Standard\data\infantry\shirt_co.paa",
+			"34thPRC_Armour_Standard\data\infantry\legs_co.paa",
+			"34thPRC_Armour_Standard\data\infantry\shoulders2_co.paa"
+		};
+		uniformClass="34thPRC_Armour_Standard_Trooper_Rifleman_Urban2";
+	}
+}
+
+class 34thPRC_Armour_Standard_Uniform_Rifleman_Urban3 : haloinfmar_U_GRFS_uniform
+{
+	displayName="[34th] Uniform3";
+	author="Cappy";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_Armour_Standard\data\infantry\shirt_co.paa",
+		"34thPRC_Armour_Standard\data\infantry\legs_co.paa",
+		"34thPRC_Armour_Standard\data\infantry\shoulders3_co.paa"
+	};
+	class ItemInfo : ItemInfo
+	{
+		hiddenSelectionsTextures[]=
+		{
+			"34thPRC_Armour_Standard\data\infantry\shirt_co.paa",
+			"34thPRC_Armour_Standard\data\infantry\legs_co.paa",
+			"34thPRC_Armour_Standard\data\infantry\shoulders3_co.paa"
+		};
+		uniformClass="34thPRC_Armour_Standard_Trooper_Rifleman_Urban3";
+	}
+}
+
+class 34thPRC_Armour_Standard_Uniform_Rifleman_Urban4 : haloinfmar_U_GRFS_uniform
+{
+	displayName="[34th] Uniform4";
+	author="Cappy";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_Armour_Standard\data\infantry\shirt_co.paa",
+		"34thPRC_Armour_Standard\data\infantry\legs_co.paa",
+		"34thPRC_Armour_Standard\data\infantry\shoulders4_co.paa"
+	};
+	class ItemInfo : ItemInfo
+	{
+		hiddenSelectionsTextures[]=
+		{
+			"34thPRC_Armour_Standard\data\infantry\shirt_co.paa",
+			"34thPRC_Armour_Standard\data\infantry\legs_co.paa",
+			"34thPRC_Armour_Standard\data\infantry\shoulders4_co.paa"
+		};
+		uniformClass="34thPRC_Armour_Standard_Trooper_Rifleman_Urban4";
+	}
+}
