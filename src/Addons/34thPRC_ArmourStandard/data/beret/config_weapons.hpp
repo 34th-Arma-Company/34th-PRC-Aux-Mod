2a8052038d90867244e964c7c8025ac9fd11f71a
Lupus590 <lupussolitarius590@gmail.com>
2024-02-15 22:27:16 +0000

    34thPRC_Armour_Standard -> 34thPRC_ArmourStandard

--- src/Addons/34thPRC_ArmourStandard/data/beret/config_weapons.hpp
+++ src/Addons/34thPRC_ArmourStandard/data/beret/config_weapons.hpp
@@ -1,5 +1,5 @@
 class H_Beret_02; // A3_Characters_F_EPB
-class 34thPRC_Armour_Standard_Beret_01: H_Beret_02
+class 34thPRC_ArmourStandard_Beret_01: H_Beret_02
 {
 	displayName="[34th] Beret 01";
 	author="Lupus590";
@@ -8,15 +8,15 @@ class 34thPRC_Armour_Standard_Beret_01: H_Beret_02
 	scopeCurator=2;
 	hiddenSelectionsTextures[]=
 	{
-		"34thPRC_Armour_Standard\data\beret\beret_01.paa",
+		"34thPRC_ArmourStandard\data\beret\beret_01.paa",
 	};
 };
-class 34thPRC_Armour_Standard_Beret_02: 34thPRC_Armour_Standard_Beret_01
+class 34thPRC_ArmourStandard_Beret_02: 34thPRC_ArmourStandard_Beret_01
 {
 	displayName="[34th] Beret 02";
 	author="Lupus590";
 	hiddenSelectionsTextures[]=
 	{
-		"34thPRC_Armour_Standard\data\beret\beret_02.paa",
+		"34thPRC_ArmourStandard\data\beret\beret_02.paa",
 	};
 };
---
+++ src/Addons/34thPRC_ArmourStandard/data/marines/config_vehicles.hpp
@@ -0,0 +1,146 @@
+class HaloInf_Marine_GRFS_F; // Halo_marine_02
+class 34thPRC_ArmourStandard_Marines_Trooper_Urban_Blue_Medium : HaloInf_Marine_GRFS_F
+{
+	scope=1;
+	displayName="[34th] Trooper Urban Blue Medium";
+	author="TheBrwnKidd";
+	uniformClass="34thPRC_ArmourStandard_Marines_Uniform_Urban_Blue_Medium";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_ArmourStandard\data\marines\urban\shirt_urban_blue_medium_co.paa",
+		"34thPRC_ArmourStandard\data\marines\urban\legs_urban_blue_medium_co.paa",
+		"34thPRC_ArmourStandard\data\marines\urban\shoulders_urban_co.paa"
+	};
+}
+class 34thPRC_ArmourStandard_Marines_Trooper_Urban_Blue_Light : 34thPRC_ArmourStandard_Marines_Trooper_Urban_Blue_Medium
+{
+	displayName="[34th] Trooper Urban Blue Light";
+	author="TheBrwnKidd";
+	uniformClass="34thPRC_ArmourStandard_Marines_Uniform_Urban_Blue_Light";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_ArmourStandard\data\marines\urban\shirt_urban_blue_light_co.paa",
+		"34thPRC_ArmourStandard\data\marines\urban\legs_urban_blue_light_co.paa",
+		"34thPRC_ArmourStandard\data\marines\urban\shoulders_urban_co.paa"
+	};
+}
+class 34thPRC_ArmourStandard_Marines_Trooper_Urban_Blue_Dark : 34thPRC_ArmourStandard_Marines_Trooper_Urban_Blue_Medium
+{
+	displayName="[34th] Trooper Urban Blue Dark";
+	author="TheBrwnKidd";
+	uniformClass="34thPRC_ArmourStandard_Marines_Uniform_Urban_Blue_Dark";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_ArmourStandard\data\marines\urban\shirt_urban_blue_dark_co.paa",
+		"34thPRC_ArmourStandard\data\marines\urban\legs_urban_blue_dark_co.paa",
+		"34thPRC_ArmourStandard\data\marines\urban\shoulders_urban_co.paa"
+	};
+}
+class 34thPRC_ArmourStandard_Marines_Trooper_Urban_Red_Light : 34thPRC_ArmourStandard_Marines_Trooper_Urban_Blue_Medium
+{
+	displayName="[34th] Trooper Urban Red Light";
+	author="TheBrwnKidd";
+	uniformClass="34thPRC_ArmourStandard_Marines_Uniform_Urban_Red_Light";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_ArmourStandard\data\marines\urban\shirt_urban_red_light_co.paa",
+		"34thPRC_ArmourStandard\data\marines\urban\legs_urban_red_light_co.paa",
+		"34thPRC_ArmourStandard\data\marines\urban\shoulders_urban_co.paa"
+	};
+}
+class 34thPRC_ArmourStandard_Marines_Trooper_Urban_Red_Dark : 34thPRC_ArmourStandard_Marines_Trooper_Urban_Blue_Medium
+{
+	displayName="[34th] Trooper Urban Red Dark";
+	author="TheBrwnKidd";
+	uniformClass="34thPRC_ArmourStandard_Marines_Uniform_Urban_Red_Dark";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_ArmourStandard\data\marines\urban\shirt_urban_red_dark_co.paa",
+		"34thPRC_ArmourStandard\data\marines\urban\legs_urban_red_dark_co.paa",
+		"34thPRC_ArmourStandard\data\marines\urban\shoulders_urban_co.paa"
+	};
+}
+class 34thPRC_ArmourStandard_Marines_Trooper_Arctic_Light : 34thPRC_ArmourStandard_Marines_Trooper_Urban_Blue_Medium
+{
+	displayName="[34th] Trooper Arctic Light";
+	author="TheBrwnKidd";
+	uniformClass="34thPRC_ArmourStandard_Marines_Uniform_Arctic_Light";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_ArmourStandard\data\marines\arctic\shirt_arctic_light_co.paa",
+		"34thPRC_ArmourStandard\data\marines\arctic\legs_arctic_light_co.paa",
+		"34thPRC_ArmourStandard\data\marines\arctic\shoulders_arctic_light_co.paa"
+	};
+}
+class 34thPRC_ArmourStandard_Marines_Trooper_Arctic_Dark : 34thPRC_ArmourStandard_Marines_Trooper_Urban_Blue_Medium
+{
+	displayName="[34th] Trooper Arctic Dark";
+	author="TheBrwnKidd";
+	uniformClass="34thPRC_ArmourStandard_Marines_Uniform_Arctic_Dark";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_ArmourStandard\data\marines\arctic\shirt_arctic_dark_co.paa",
+		"34thPRC_ArmourStandard\data\marines\arctic\legs_arctic_dark_co.paa",
+		"34thPRC_ArmourStandard\data\marines\arctic\shoulders_arctic_dark_co.paa"
+	};
+}
+class 34thPRC_ArmourStandard_Marines_Trooper_Arid_Light : 34thPRC_ArmourStandard_Marines_Trooper_Urban_Blue_Medium
+{
+	displayName="[34th] Trooper Arid Light";
+	author="TheBrwnKidd";
+	uniformClass="34thPRC_ArmourStandard_Marines_Uniform_Arid_Light";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_ArmourStandard\data\marines\arid\shirt_arid_light_co.paa",
+		"34thPRC_ArmourStandard\data\marines\arid\legs_arid_light_co.paa",
+		"34thPRC_ArmourStandard\data\marines\arid\shoulders_arid_light_co.paa"
+	};
+}
+class 34thPRC_ArmourStandard_Marines_Trooper_Arid_Dark : 34thPRC_ArmourStandard_Marines_Trooper_Urban_Blue_Medium
+{
+	displayName="[34th] Trooper Arid Dark";
+	author="TheBrwnKidd";
+	uniformClass="34thPRC_ArmourStandard_Marines_Uniform_Arid_Dark";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_ArmourStandard\data\marines\arid\shirt_arid_dark_co.paa",
+		"34thPRC_ArmourStandard\data\marines\arid\legs_arid_dark_co.paa",
+		"34thPRC_ArmourStandard\data\marines\arid\shoulders_arid_dark_co.paa"
+	};
+}
+class 34thPRC_ArmourStandard_Marines_Trooper_Woodland_Light : 34thPRC_ArmourStandard_Marines_Trooper_Urban_Blue_Medium
+{
+	displayName="[34th] Trooper Woodland Light";
+	author="TheBrwnKidd";
+	uniformClass="34thPRC_ArmourStandard_Marines_Uniform_Woodland_Light";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_ArmourStandard\data\marines\woodland\shirt_woodland_light_co.paa",
+		"34thPRC_ArmourStandard\data\marines\woodland\legs_woodland_light_co.paa",
+		"34thPRC_ArmourStandard\data\marines\woodland\shoulders_woodland_light_co.paa"
+	};
+}
+class 34thPRC_ArmourStandard_Marines_Trooper_Woodland_Dark : 34thPRC_ArmourStandard_Marines_Trooper_Urban_Blue_Medium
+{
+	displayName="[34th] Trooper Woodland Dark";
+	author="TheBrwnKidd";
+	uniformClass="34thPRC_ArmourStandard_Marines_Uniform_Woodland_Dark";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_ArmourStandard\data\marines\woodland\shirt_woodland_dark_co.paa",
+		"34thPRC_ArmourStandard\data\marines\woodland\legs_woodland_dark_co.paa",
+		"34thPRC_ArmourStandard\data\marines\woodland\shoulders_woodland_dark_co.paa"
+	};
+}
+class 34thPRC_ArmourStandard_Marines_Trooper_Tropic : 34thPRC_ArmourStandard_Marines_Trooper_Urban_Blue_Medium
+{
+	displayName="[34th] Trooper Tropic";
+	author="TheBrwnKidd";
+	uniformClass="34thPRC_ArmourStandard_Marines_Uniform_Tropic";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_ArmourStandard\data\marines\tropic\shirt_tropic_co.paa",
+		"34thPRC_ArmourStandard\data\marines\tropic\legs_tropic_co.paa",
+		"34thPRC_ArmourStandard\data\marines\tropic\shoulders_tropic_co.paa"
+	};
+}
