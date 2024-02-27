76dd4e434da471c4f0fbb4bb5a533e3b5343d061
Lupus590 <lupussolitarius590@gmail.com>
2024-02-27 22:31:10 +0000

    No active camo yet

--- src/Addons/34thPRC_ArmourStandard/data/spi/config_vehicles.hpp
+++ src/Addons/34thPRC_ArmourStandard/data/spi/config_vehicles.hpp
@@ -14,7 +14,7 @@ class CAManBase: Man
 			hideOnUse=1;
 			onlyForPlayer=0;
 			shortcut="";
-			condition="WBK_EPSM_CloakEnabled && !WBK_EPSM_CompletlyDisable && missionNamespace getVariable['bis_fnc_moduleRemoteControl_unit', player] == this && alive this && gestureState this != 'Exo_Gest_Heal_Armor' && backpack this == '34thPRC_Armour_Standard_Backpack_StealthExoSuit_Invisible' && !(isNil {this getVariable 'WBK_AdvancedArmor'}) && isNil {this getVariable 'WBK_StealthPower'}";
+			condition="WBK_EPSM_CloakEnabled && !WBK_EPSM_CompletlyDisable && missionNamespace getVariable['bis_fnc_moduleRemoteControl_unit', player] == this && alive this && gestureState this != 'Exo_Gest_Heal_Armor' && backpack this == '34thPRC_ArmourStandard_Exosuit_InvisibleStealth' && !(isNil {this getVariable 'WBK_AdvancedArmor'}) && isNil {this getVariable 'WBK_StealthPower'}";
 			statement="this spawn WBK_StealthMechanic;";
 		};
 	};
