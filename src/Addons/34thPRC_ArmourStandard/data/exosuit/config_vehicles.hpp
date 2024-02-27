eb2ac0b1e036c3f7c5bd99f4f92818924ad1f05f
Lupus590 <lupussolitarius590@gmail.com>
2024-02-27 23:06:53 +0000

    Rename and white space

--- src/Addons/34thPRC_ArmourStandard/data/exosuit/config_vehicles.hpp
+++ src/Addons/34thPRC_ArmourStandard/data/exosuit/config_vehicles.hpp
@@ -1,7 +1,8 @@
 class Sinagami_Assault_Exo; // PhoenixSystems_Exosuits
+
 class 34thPRC_ArmourStandard_Exosuit_InvisibleStealth: Sinagami_Assault_Exo
 {
-		displayName="Exoskeleton SG-1 Stealth (Invisible)"; // SPI Stealth and Communication Suite
-		author="Lupus590";
-		model="34thPRC_ArmourStandard/null.p3d";
+	displayName="SPI Stealth and Communication Suite";
+	author="Lupus590";
+	model="34thPRC_ArmourStandard/null.p3d";
 }
