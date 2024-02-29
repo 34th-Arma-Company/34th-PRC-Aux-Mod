f20e71a95814b59dd18662eb08de3a397beaca18
Vazya <161765906+Vazya-H3lljumper@users.noreply.github.com>
2024-02-29 17:15:16 -0600

    Add basic pilot helmet
    Added 34th entry for pilot helmet
    Still using UNSC Foundries textures

---
+++ src/Addons/34thPRC_ArmourStandard/data/aircrew/config_weapons.hpp
@@ -0,0 +1,15 @@
+class DMNS_IHADSS_HELMET_01; // DMNS_Unit_Headgear
+class 34thPRC_ArmourStandard_Aircrew_Helmet: DMNS_IHADSS_HELMET_01
+{
+	displayName="[34th] IHADSS Pilot Helmet";
+	author="Vazya";
+	hiddenSelectionsTextures[]=
+	{
+		//"34thPRC_ArmourStandard\data\aircrew\aircrew_helmet.paa",
+		"\DMNS\DMNS_Units\Armour\Pilot_Helm_01\Data\H2A_Pilot_Helm_Addons_co.paa",
+		"\DMNS\DMNS_Units\Armour\Pilot_Helm_01\Data\H2A_Pilot_Helm_EXT_co.paa",
+		"\DMNS\DMNS_Units\Armour\Pilot_Helm_01\Data\H2A_Pilot_Helm_INT_co.paa",
+		"\DMNS\DMNS_Units\Armour\Pilot_Helm_01\Data\H2A_Pilot_Helm_MID_co.paa",
+		"\DMNS\DMNS_Units\Armour\Pilot_Helm_01\Data\H2A_Pilot_Helmet_Visor_co.paa"
+	};
+};
--- src/Addons/34thPRC_ArmourStandard/data/config_weapons.hpp
+++ src/Addons/34thPRC_ArmourStandard/data/config_weapons.hpp
@@ -1,3 +1,4 @@
+#include "aircrew\config_weapons.hpp"
 #include "beret\config_weapons.hpp"
 #include "marines\config_weapons.hpp"
 #include "odst\config_weapons.hpp"
