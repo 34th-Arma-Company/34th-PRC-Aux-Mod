dad23e640a4d2c2f30c2462602877729f88ed6e1
Lupus590 <lupussolitarius590@gmail.com>
2024-02-29 19:53:08 +0000

    Webknight opensource

--- src/Addons/34thPRC_ArmourStandard/config.cpp
+++ src/Addons/34thPRC_ArmourStandard/config.cpp
@@ -23,3 +23,38 @@ class CfgWeapons
 {
 	#include "data\config_weapons.hpp"
 };
+
+class Extended_InitPost_EventHandlers {
+    class CAManBase {
+        class EPSM_ExoMod_SoldierInit {
+			onRespawn="true";
+            init = "_unit = _this select 0; if (local _unit) then {_unit setVariable [""WBK_AdvancedArmor"",100,true]; _unit setVariable [""WBK_JumpPackPower"",100]; _unit setVariable [""IMS_SoundObject"",nil,true]; _unit spawn WBK_EPSM_AdvancedArmour_Load;};";
+		};
+    };
+};
+
+class Extended_HitPart_EventHandlers {
+    class CAManBase {
+        class EPSM_ExoMod_SoldierHitPart {
+			onRespawn="true";
+            HitPart = "_this spawn WBK_EPSM_AdvancedArmour;";
+        };
+    };
+};
+
+class Extended_PreInit_EventHandlers
+{
+    class EPSM_PreInit
+    {
+        init="call compile preprocessFileLineNumbers '\ExoSystem_WBK\scripts\XEH_preInit.sqf'";
+    };
+};
+
+
+class Extended_PostInit_EventHandlers
+{
+    class EPSM_PostInit
+	{
+        init="call compile preprocessFileLineNumbers '\ExoSystem_WBK\scripts\XEH_postInit.sqf'";
+    };
+};
