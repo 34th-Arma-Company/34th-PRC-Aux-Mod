dc9237755d831c8fd499f95edc8ed13b70bcac8e
Lupus590 <lupussolitarius590@gmail.com>
2024-07-31 20:55:45 +0100

    WIP

---
+++ src/Addons/34thPRC_ArmourStandard/data/marines2/config_weapons.hpp
@@ -0,0 +1,12 @@
+class CH252_Helmet_Standard;
+class CH252_Helmet_EyePiece;
+class CH252_Helmet_VisorGlasses;
+
+class MA_Marine_BDU_Heavy;
+class MA_Marine_BDU_Light;
+class MA_Marine_BDU_Medium;
+
+// no collar
+class MA_Marine_BDU_Heavy_NC;
+class MA_Marine_BDU_Light_NC;
+class MA_Marine_BDU_Medium_NC;
