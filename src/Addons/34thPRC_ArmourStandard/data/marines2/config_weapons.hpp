1c2ef89a0cdf7cd6de95758c22f0b251b5c622de
Lupus590 <lupussolitarius590@gmail.com>
2024-08-25 22:32:25 +0100

    Helmets config scaffold
    Still need to map textures to classes

--- src/Addons/34thPRC_ArmourStandard/data/marines2/config_weapons.hpp
+++ src/Addons/34thPRC_ArmourStandard/data/marines2/config_weapons.hpp
@@ -2,6 +2,307 @@ class CH252_Helmet_Standard;
 class CH252_Helmet_EyePiece;
 class CH252_Helmet_VisorGlasses;
 
+class 34thPRC_ArmourStandard_Marines2_CH252_Urban : CH252_Helmet_Standard
+{
+	displayName="[34th] CH252-2 Urban";
+	author=""; // TODO: Author
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_ArmourStandard\data\marines\urban\helmet_urban_co.paa"
+	};
+}
+
+class 34thPRC_ArmourStandard_Marines2_CH252_Urban_EyePiece : CH252_Helmet_EyePiece
+{
+	displayName="[34th] CH252-2 Urban (Eye Piece)";
+	author="";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_ArmourStandard\data\marines\urban\helmet_urban_co.paa"
+	};
+}
+
+class 34thPRC_ArmourStandard_Marines2_CH252_Urban_VisorGlasses : CH252_Helmet_VisorGlasses
+{
+	displayName="[34th] CH252-2 Urban (Visor Glasses)";
+	author="";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_ArmourStandard\data\marines\urban\helmet_urban_co.paa"
+	};
+}
+
+class 34thPRC_ArmourStandard_Marines2_CH252_Arctic : 34thPRC_ArmourStandard_Marines2_CH252_Urban
+{
+	displayName="[34th] CH252-2 Arctic";
+	author="";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_ArmourStandard\data\marines\urban\helmet_urban_co.paa"
+	};
+}
+
+class 34thPRC_ArmourStandard_Marines2_CH252_Arctic_EyePiece : 34thPRC_ArmourStandard_Marines2_CH252_Urban_EyePiece
+{
+	displayName="[34th] CH252-2 Arctic (Eye Piece)";
+	author="";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_ArmourStandard\data\marines\urban\helmet_urban_co.paa"
+	};
+}
+
+class 34thPRC_ArmourStandard_Marines2_CH252_Arctic_VisorGlasses : 34thPRC_ArmourStandard_Marines2_CH252_Urban_VisorGlasses
+{
+	displayName="[34th] CH252-2 Arctic (Visor Glasses)";
+	author="";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_ArmourStandard\data\marines\urban\helmet_urban_co.paa"
+	};
+}
+
+class 34thPRC_ArmourStandard_Marines2_CH252_Arid : 34thPRC_ArmourStandard_Marines2_CH252_Urban
+{
+	displayName="[34th] CH252-2 Arid";
+	author="";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_ArmourStandard\data\marines\urban\helmet_urban_co.paa"
+	};
+}
+
+class 34thPRC_ArmourStandard_Marines2_CH252_Arid_EyePiece : 34thPRC_ArmourStandard_Marines2_CH252_Urban_EyePiece
+{
+	displayName="[34th] CH252-2 Arid (Eye Piece)";
+	author="";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_ArmourStandard\data\marines\urban\helmet_urban_co.paa"
+	};
+}
+
+class 34thPRC_ArmourStandard_Marines2_CH252_Arid_VisorGlasses : 34thPRC_ArmourStandard_Marines2_CH252_Urban_VisorGlasses
+{
+	displayName="[34th] CH252-2 Arid (Visor Glasses)";
+	author="";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_ArmourStandard\data\marines\urban\helmet_urban_co.paa"
+	};
+}
+
+class 34thPRC_ArmourStandard_Marines2_CH252_Tropic : 34thPRC_ArmourStandard_Marines2_CH252_Urban
+{
+	displayName="[34th] CH252-2 Tropic";
+	author="";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_ArmourStandard\data\marines\urban\helmet_urban_co.paa"
+	};
+}
+
+class 34thPRC_ArmourStandard_Marines2_CH252_Tropic_EyePiece : 34thPRC_ArmourStandard_Marines2_CH252_Urban_EyePiece
+{
+	displayName="[34th] CH252-2 Tropic (Eye Piece)";
+	author="";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_ArmourStandard\data\marines\urban\helmet_urban_co.paa"
+	};
+}
+
+class 34thPRC_ArmourStandard_Marines2_CH252_Tropic_VisorGlasses : 34thPRC_ArmourStandard_Marines2_CH252_Urban_VisorGlasses
+{
+	displayName="[34th] CH252-2 Tropic (Visor Glasses)";
+	author="";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_ArmourStandard\data\marines\urban\helmet_urban_co.paa"
+	};
+}
+
+class 34thPRC_ArmourStandard_Marines2_CH252_Woodland : 34thPRC_ArmourStandard_Marines2_CH252_Urban
+{
+	displayName="[34th] CH252-2 Woodland";
+	author="";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_ArmourStandard\data\marines\urban\helmet_urban_co.paa"
+	};
+}
+
+class 34thPRC_ArmourStandard_Marines2_CH252_Woodland_EyePiece : 34thPRC_ArmourStandard_Marines2_CH252_Urban_EyePiece
+{
+	displayName="[34th] CH252-2 Woodland (Eye Piece)";
+	author="";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_ArmourStandard\data\marines\urban\helmet_urban_co.paa"
+	};
+}
+
+class 34thPRC_ArmourStandard_Marines2_CH252_Woodland_VisorGlasses : 34thPRC_ArmourStandard_Marines2_CH252_Urban_VisorGlasses
+{
+	displayName="[34th] CH252-2 Woodland (Visor Glasses)";
+	author="";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_ArmourStandard\data\marines\urban\helmet_urban_co.paa"
+	};
+}
+
+class 34thPRC_ArmourStandard_Marines2_CH252_Urban_Medic : CH252_Helmet_Standard
+{
+	displayName="[34th] CH252-2 Urban (Medic)";
+	author="";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_ArmourStandard\data\marines\urban\helmet_urban_co.paa"
+	};
+}
+
+class 34thPRC_ArmourStandard_Marines2_CH252_Urban_EyePiece_Medic : CH252_Helmet_EyePiece
+{
+	displayName="[34th] CH252-2 Urban (Eye Piece) (Medic)";
+	author="";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_ArmourStandard\data\marines\urban\helmet_urban_co.paa"
+	};
+}
+
+class 34thPRC_ArmourStandard_Marines2_CH252_Urban_VisorGlasses_Medic : CH252_Helmet_VisorGlasses
+{
+	displayName="[34th] CH252-2 Urban (Visor Glasses) (Medic)";
+	author="";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_ArmourStandard\data\marines\urban\helmet_urban_co.paa"
+	};
+}
+
+class 34thPRC_ArmourStandard_Marines2_CH252_Arctic_Medic : 34thPRC_ArmourStandard_Marines2_CH252_Urban_Medic
+{
+	displayName="[34th] CH252-2 Arctic (Medic)";
+	author="";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_ArmourStandard\data\marines\urban\helmet_urban_co.paa"
+	};
+}
+
+class 34thPRC_ArmourStandard_Marines2_CH252_Arctic_EyePiece_Medic : 34thPRC_ArmourStandard_Marines2_CH252_Urban_EyePiece_Medic
+{
+	displayName="[34th] CH252-2 Arctic (Eye Piece) (Medic)";
+	author="";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_ArmourStandard\data\marines\urban\helmet_urban_co.paa"
+	};
+}
+
+class 34thPRC_ArmourStandard_Marines2_CH252_Arctic_VisorGlasses_Medic : 34thPRC_ArmourStandard_Marines2_CH252_Urban_VisorGlasses_Medic
+{
+	displayName="[34th] CH252-2 Arctic (Visor Glasses) (Medic)";
+	author="";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_ArmourStandard\data\marines\urban\helmet_urban_co.paa"
+	};
+}
+
+class 34thPRC_ArmourStandard_Marines2_CH252_Arid_Medic : 34thPRC_ArmourStandard_Marines2_CH252_Urban_Medic
+{
+	displayName="[34th] CH252-2 Arid (Medic)";
+	author="";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_ArmourStandard\data\marines\urban\helmet_urban_co.paa"
+	};
+}
+
+class 34thPRC_ArmourStandard_Marines2_CH252_Arid_EyePiece_Medic : 34thPRC_ArmourStandard_Marines2_CH252_Urban_EyePiece_Medic
+{
+	displayName="[34th] CH252-2 Arid (Eye Piece) (Medic)";
+	author="";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_ArmourStandard\data\marines\urban\helmet_urban_co.paa"
+	};
+}
+
+class 34thPRC_ArmourStandard_Marines2_CH252_Arid_VisorGlasses_Medic : 34thPRC_ArmourStandard_Marines2_CH252_Urban_VisorGlasses_Medic
+{
+	displayName="[34th] CH252-2 Arid (Visor Glasses) (Medic)";
+	author="";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_ArmourStandard\data\marines\urban\helmet_urban_co.paa"
+	};
+}
+
+class 34thPRC_ArmourStandard_Marines2_CH252_Tropic_Medic : 34thPRC_ArmourStandard_Marines2_CH252_Urban_Medic
+{
+	displayName="[34th] CH252-2 Tropic (Medic)";
+	author="";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_ArmourStandard\data\marines\urban\helmet_urban_co.paa"
+	};
+}
+
+class 34thPRC_ArmourStandard_Marines2_CH252_Tropic_EyePiece_Medic : 34thPRC_ArmourStandard_Marines2_CH252_Urban_EyePiece_Medic
+{
+	displayName="[34th] CH252-2 Tropic (Eye Piece) (Medic)";
+	author="";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_ArmourStandard\data\marines\urban\helmet_urban_co.paa"
+	};
+}
+
+class 34thPRC_ArmourStandard_Marines2_CH252_Tropic_VisorGlasses_Medic : 34thPRC_ArmourStandard_Marines2_CH252_Urban_VisorGlasses_Medic
+{
+	displayName="[34th] CH252-2 Tropic (Visor Glasses) (Medic)";
+	author="";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_ArmourStandard\data\marines\urban\helmet_urban_co.paa"
+	};
+}
+
+class 34thPRC_ArmourStandard_Marines2_CH252_Woodland_Medic : 34thPRC_ArmourStandard_Marines2_CH252_Urban_Medic
+{
+	displayName="[34th] CH252-2 Woodland (Medic)";
+	author="";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_ArmourStandard\data\marines\urban\helmet_urban_co.paa"
+	};
+}
+
+class 34thPRC_ArmourStandard_Marines2_CH252_Woodland_EyePiece_Medic : 34thPRC_ArmourStandard_Marines2_CH252_Urban_EyePiece_Medic
+{
+	displayName="[34th] CH252-2 Woodland (Eye Piece) (Medic)";
+	author="";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_ArmourStandard\data\marines\urban\helmet_urban_co.paa"
+	};
+}
+
+class 34thPRC_ArmourStandard_Marines2_CH252_Woodland_VisorGlasses_Medic : 34thPRC_ArmourStandard_Marines2_CH252_Urban_VisorGlasses_Medic
+{
+	displayName="[34th] CH252-2 Woodland (Visor Glasses) (Medic)";
+	author="";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_ArmourStandard\data\marines\urban\helmet_urban_co.paa"
+	};
+}
+
+
 class MA_Marine_BDU_Heavy;
 class MA_Marine_BDU_Light;
 class MA_Marine_BDU_Medium;
