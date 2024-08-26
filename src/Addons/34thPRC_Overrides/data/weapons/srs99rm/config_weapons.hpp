b919f8471bd68def0e9abd721f3ee07b8a1c11d5
Lupus590 <lupussolitarius590@gmail.com>
2024-08-27 00:50:00 +0100

    Thank you SEC for donating these configs

--- src/Addons/34thPRC_Overrides/data/weapons/srs99rm/config_weapons.hpp
+++ src/Addons/34thPRC_Overrides/data/weapons/srs99rm/config_weapons.hpp
@@ -1,6 +1,6 @@
 class arifle_19_UNSC_SRS99AM_Base_F; // 19_UNSC_Weapons
-class WeaponSlotsInfo;
-class MuzzleSlot;
+// class WeaponSlotsInfo;
+// class MuzzleSlot;
 class 19_UNSC_SRS99AM : arifle_19_UNSC_SRS99AM_Base_F
 {
 	class WeaponSlotsInfo: WeaponSlotsInfo
