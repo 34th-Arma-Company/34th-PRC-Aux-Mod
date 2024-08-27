8335a7faf2738907551f72bd14ff1f332affe99c
Lupus590 <lupussolitarius590@gmail.com>
2024-08-27 21:57:57 +0100

    Mark our mags with our display name tag

--- src/Addons/34thPRC_Overrides/data/weapons/grl45/config_magazines.hpp
+++ src/Addons/34thPRC_Overrides/data/weapons/grl45/config_magazines.hpp
@@ -1,7 +1,7 @@
 class GRL45_6rnd_Flare_White; // Misriah_Armory_Weapons
 class 34thPRC_Overrides_Weapons_Grl45_Flare_White: GRL45_6rnd_Flare_White
 {
-	displayName="6Rnd White Illumination Flares";
+	displayName="[34th] 6Rnd White Illumination Flares";
 	displayNameShort="White Flare";
 	descriptionShort="6 Round White Illumination Flares";
 	ammo = "ACE_40mm_Flare_white"; // ace_grenades
@@ -9,7 +9,7 @@ class 34thPRC_Overrides_Weapons_Grl45_Flare_White: GRL45_6rnd_Flare_White
 class GRL45_6rnd_Flare_Red; // Misriah_Armory_Weapons
 class 34thPRC_Overrides_Weapons_Grl45_Flare_Red: GRL45_6rnd_Flare_Red
 {
-	displayName="6Rnd Red Illumination Flares";
+	displayName="[34th] 6Rnd Red Illumination Flares";
 	displayNameShort="Red Flare";
 	descriptionShort="6 Round Red Illumination Flares";
 	ammo = "ACE_40mm_Flare_red"; // ace_grenades
@@ -17,7 +17,7 @@ class 34thPRC_Overrides_Weapons_Grl45_Flare_Red: GRL45_6rnd_Flare_Red
 class GRL45_6rnd_Flare_Green; // Misriah_Armory_Weapons
 class 34thPRC_Overrides_Weapons_Grl45_Flare_Green: GRL45_6rnd_Flare_Green
 {
-	displayName="6Rnd Green Illumination Flares";
+	displayName="[34th] 6Rnd Green Illumination Flares";
 	displayNameShort="Green Flare"
 	descriptionShort="6 Round Green Illumination Flares";
 	ammo = "ACE_40mm_Flare_green"; // ace_grenades
