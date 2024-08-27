18ee953d1736cf734969f509a0a187543350ad5b
Lupus590 <lupussolitarius590@gmail.com>
2024-08-27 21:32:47 +0100

    With better names now

--- src/Addons/34thPRC_Overrides/data/weapons/grl45/config_magazines.hpp
+++ src/Addons/34thPRC_Overrides/data/weapons/grl45/config_magazines.hpp
@@ -1,5 +1,8 @@
 class GRL45_6rnd_Flare_White;
 class 34thPRC_Overrides_Weapons_Grl45_Flare_white: GRL45_6rnd_Flare_White
 {
+	displayName="6Rnd White Illumination Flares";
+	displayNameShort="White Flare";
+	descriptionShort="6 Round White Illumination Flares";
 	ammo = "ACE_40mm_Flare_white"; // ace_grenades
 };
