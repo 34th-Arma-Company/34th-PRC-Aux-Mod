e25960eeb32559893322e68bd281d27878142bc1
Lupus590 <lupussolitarius590@gmail.com>
2024-08-27 13:35:54 +0100

    Weapons Attachements Additions (Part 1) & Bigger Spartan Undersuits (#168)
    Thank you SEC for donating these configs

--- src/Addons/34thPRC_Overrides/config.cpp
+++ src/Addons/34thPRC_Overrides/config.cpp
@@ -6,8 +6,12 @@ class CfgPatches
 		units[]={};
 		requiredAddons[] =
 		{
+			"A3_Characters_F", // Vanilla
 			"DMNS_Vehicles_Bulldog", // UNSC Foundries
-			"DMNS_Vehicles_M511_Springbok" // UNSC Foundries
+			"DMNS_Vehicles_M511_Springbok", // UNSC Foundries
+			"19_UNSC_Weapons", // UNSC Foundries
+			"OPTRE_UNSC_Units_Army", // OPTRE
+			"OPTRE_FC_Units_Marines" // OPTRE FC
 		};
 	};
 };
---
+++ src/Addons/34thPRC_Overrides/data/weapons/config_weapons.hpp
@@ -0,0 +1,5 @@
+#include "br55\config_weapons.hpp"
+#include "m392\config_weapons.hpp"
+#include "ma5a\config_weapons.hpp"
+#include "ma5b\config_weapons.hpp"
+#include "srs99rm\config_weapons.hpp"
