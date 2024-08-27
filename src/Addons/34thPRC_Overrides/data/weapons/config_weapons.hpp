72c5a2916c749195c7ffbf05511a943c007d6a60
Lupus590 <lupussolitarius590@gmail.com>
2024-08-27 02:18:39 +0100

    Tidy up

--- src/Addons/34thPRC_Overrides/data/weapons/config_weapons.hpp
+++ src/Addons/34thPRC_Overrides/data/weapons/config_weapons.hpp
@@ -1,6 +1,4 @@
 #include "br55\config_weapons.hpp"
-#include "bulldog\config_weapons.hpp"
-#include "grl45\config_weapons.hpp"
 #include "m392\config_weapons.hpp"
 #include "ma5a\config_weapons.hpp"
 #include "ma5b\config_weapons.hpp"
