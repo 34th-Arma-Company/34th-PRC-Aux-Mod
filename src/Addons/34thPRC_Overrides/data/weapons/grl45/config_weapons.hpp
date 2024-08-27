d5069de67c08b887124ca5a3303a90c6c10a37a6
Lupus590 <lupussolitarius590@gmail.com>
2024-08-27 21:53:03 +0100

    More colours

--- src/Addons/34thPRC_Overrides/data/weapons/grl45/config_weapons.hpp
+++ src/Addons/34thPRC_Overrides/data/weapons/grl45/config_weapons.hpp
@@ -1,8 +1,11 @@
-class Rifle_Base_F; // OPTRE_Weapons
+class Rifle_Base_F; // vanilla?
 class MA_GRL_45 : Rifle_Base_F // Misriah_Armory_Weapons
 {
 	magazines[]+=
 	{
-		"34thPRC_Overrides_Weapons_Grl45_Flare_white"
+		"34thPRC_Overrides_Weapons_Grl45_Flare_White",
+		"34thPRC_Overrides_Weapons_Grl45_Flare_Red",
+		"34thPRC_Overrides_Weapons_Grl45_Flare_Green"
+
 	};
 };
