269596646578d4ba73bac84aeea2cc976bc5b70c
conex1 <64256258+conex1@users.noreply.github.com>
2024-09-15 12:48:15 -0500

    UNSC Styled Gunbag (#206)

---
+++ src/Addons/34thPRC_Overrides/data/backpacks/marines/config_vehicles.hpp
@@ -0,0 +1,6 @@
+class 19thMD_Backpack_Standard; // 19thMD_Vests_Kelp
+class 34thPRC_Overrides_Backpacks_Marines_Gunbag : 19thMD_Backpack_Standard
+{
+	displayName="[34th] Gunbag";
+	ace_gunbag=1;
+}
