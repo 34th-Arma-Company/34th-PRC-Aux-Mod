e0e84aab78df8514f45620b0dbdaa1d483529c5a
Lupus590 <lupussolitarius590@gmail.com>
2024-09-30 23:58:08 +0100

    Textures are invisible, don't know why
    Also, changing camos might be broken

---
+++ src/Addons/34thPRC_Vehicles/data/scorpion/config_vehicles.hpp
@@ -0,0 +1,20 @@
+class DMNS_M808B_F;
+class 34thPRC_Vehicles_Scorpion : DMNS_M808B_F
+{
+	displayName="[34th] M808B Scorpion MBT";
+	faction="34thPRC_FactionBrimstone_Faction";
+	editorCategory="34thPRC_FactionBrimstone_EditorCategory";
+	editorSubcategory="34thPRC_FactionBrimstone_EditorSubCategory_Tanks";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_Vehicles\data\scorpion\M808B_Hull_co.paa",
+		"34thPRC_Vehicles\data\scorpion\M808B_Turret_co.paa",
+		"34thPRC_Vehicles\data\scorpion\M808B_Track_Housing_Front_co.paa",
+		"34thPRC_Vehicles\data\scorpion\M808B_Track_Housing_Rear_co.paa",
+		"34thPRC_Vehicles\data\scorpion\M808B_Commander_co.paa",
+		"34thPRC_Vehicles\data\scorpion\M808B_Details_EXT_co.paa",
+		"34thPRC_Vehicles\data\scorpion\M808B_Details_EXT_2_co.paa",
+		"34thPRC_Vehicles\data\scorpion\M808B_Track_Int_co.paa"
+	};
+	textureList[] = {};
+}
