92f85d7985a0d7770fc74aba8377115b53afcf8c
DaCapa <dr.34dc@gmail.com>
2024-11-04 01:38:57 -0500

    New MKIV rvmats and Textures
    Textures and rvmats are here. It's all code based at this point in time to get working in game.
    Issues:
    Stuff is invisible and the head is fitted for the spartan skeleton.
    
    Pointer file to real file

--- src/Addons/34thPRC_ArmourStandard/data/mkiv/config_weapons.hpp
+++ src/Addons/34thPRC_ArmourStandard/data/mkiv/config_weapons.hpp
@@ -1,13 +1,51 @@
 class MA_Mjolnir_MKIV_Helmet; // MA_Armor
+class VestItem;
+    class ItemInfo;
+    class HeadgearItem;
+    class V_PlateCarrier1_rgr;
+    class UniformItem;
+    class H_HelmetB;
 class 34thPRC_ArmourStandard_MKIV_Helmet : MA_Mjolnir_MKIV_Helmet
 {
     displayName="[34th] Mjolnir MKIV Helmet";
     author= "Cappy";
-    hiddenSelectionsTextures[]=
+    hiddenSelections[]=
     {
-        "34thPRC_ArmourStandard\data\mkiv\34_MKIV_Helmet.paa",
-        "34thPRC_ArmourStandard\data\mkiv\MKIV_Visor_CO.paa"
+        "camo1",
+        "camo2"
+     };
+	hiddenSelectionsTextures[]=
+    {
+        "34thPRC_ArmourStandard\data\mkiv\MKIV_Helmet_34_CO.paa",
+        "34thPRC_ArmourStandard\data\mkiv\MKIV_Visor_34_CO.paa"
     };
+	hiddenSelectionsMaterials[] =
+     {
+        "34thPRC_ArmourStandard\data\mkiv\MKIV_Helmet.rvmat",
+        "34thPRC_ArmourStandard\data\mkiv\MKIV_Visor.rvmat"
+     };
+	class ItemInfo : HeadgearItem
+        {
+            uniformModel = "MA_Armor_ORION\data\Helmets\MKIV\MKIV_Helmet_Spartan.p3d";
+            mass = 50;
+            modelSides[] = { 6 };
+            allowedSlots[] = { 801,901,701,605 };
+            hiddenSelections[] =
+            {
+                "camo1",
+                "camo2"
+            };
+            hiddenSelectionsTextures[] =
+            {
+            "34thPRC_ArmourStandard\data\mkiv\MKIV_Helmet_34_CO.paa",
+        	"34thPRC_ArmourStandard\data\mkiv\MKIV_Visor_34_CO.paa"
+            };
+            hiddenSelectionsMaterials[] =
+            {
+            "34thPRC_ArmourStandard\data\mkiv\MKIV_Helmet.rvmat",
+        	"34thPRC_ArmourStandard\data\mkiv\MKIV_Visor.rvmat"
+            };
+		};
 };
 class MA_MKIV_Armor; // MA_Armor
 class 34thPRC_ArmourStandard_MKIV_Armor: MA_MKIV_Armor
@@ -15,8 +53,48 @@ class 34thPRC_ArmourStandard_MKIV_Armor: MA_MKIV_Armor
     displayName="[34th] Mjolnir MKIV Armor";
     author= "Cappy";
 	hiddenSelectionsTextures[]=
-    {
-            "34thPRC_ArmourStandard\data\mkiv\34_MKIV_Upper.paa",    //camo4 (Upper Armor)
-            "34thPRC_ArmourStandard\data\mkiv\MKIV_Lower_CO.paa",    //camo5 (Lower Armor)
+	{
+        "34thPRC_ArmourStandard\data\mkiv\MKIV_Upper_34_CO.paa",
+        "34thPRC_ArmourStandard\data\mkiv\MKIV_Lower_34_CO.paa",
+    	"34thPRC_ArmourStandard\data\mkiv\MKIV_Shoulders_34_CO.paa",
+    	"34thPRC_ArmourStandard\data\mkiv\MKIV_Shoulders_34_CO.paa"
     };
+	hiddenSelectionsMaterials[] =
+    {
+        "34thPRC_ArmourStandard\data\mkiv\MKIV_Upper.rvmat",
+        "34thPRC_ArmourStandard\data\mkiv\MKIV_Lower.rvmat",
+        "34thPRC_ArmourStandard\data\mkiv\MKIV_Shoulders.rvmat",
+        "34thPRC_ArmourStandard\data\mkiv\MKIV_Shoulders.rvmat"
+    };
+	class ItemInfo : VestItem
+    {
+    	vestType = "Rebreather";
+    	hiddenSelections[] =
+        {
+            "camo1",
+            "camo2",
+            "camo3",
+            "camo4",
+            //    "mkvb_upper",
+            //    "mkvb_lower",
+            //    "mkiv_left",
+            //    "mkiv_right"
+        };
+         hiddenSelectionsTextures[] =
+        {
+        "34thPRC_ArmourStandard\data\mkiv\MKIV_Upper_34_CO.paa",
+        "34thPRC_ArmourStandard\data\mkiv\MKIV_Lower_34_CO.paa",
+    	"34thPRC_ArmourStandard\data\mkiv\MKIV_Shoulders_34_CO.paa",
+    	"34thPRC_ArmourStandard\data\mkiv\MKIV_Shoulders_34_CO.paa"
+        };
+        hiddenSelectionsMaterials[] =
+        {
+        "34thPRC_ArmourStandard\data\mkiv\MKIV_Upper.rvmat",
+        "34thPRC_ArmourStandard\data\mkiv\MKIV_Lower.rvmat",
+        "34thPRC_ArmourStandard\data\mkiv\MKIV_Shoulders.rvmat",
+        "34thPRC_ArmourStandard\data\mkiv\MKIV_Shoulders.rvmat"
+        };
+	};
 };
+
+
---
+++ src/Addons/34thPRC_ArmourStandard/data/mkiv/mkiv_old_textures/config_vehicles.hpp
@@ -0,0 +1,36 @@
+class Mjolnir_MKIV_Shoulders; // MA_Armor
+class 34thPRC_ArmourStandard_MKIV_Shoulders : Mjolnir_MKIV_Shoulders
+{
+    displayName="[34th] Mjolnir MKIV Shoulders";
+    author="Cappy";
+    hiddenSelectionsTextures[]=
+    {
+		"MA_Armor\data\Vests\MKVB\Shoulders\Commando\Olive_Commando_Shoulders_CO.paa",
+        "MA_Armor\data\Vests\MKVB\Shoulders\Commando\Olive_Commando_Shoulders_CO.paa",
+        "MA_Armor\data\Vests\MKVB\Shoulders\CQC\Olive_CQC_Shoulders_CO.paa",
+        "MA_Armor\data\Vests\MKVB\Shoulders\CQC\Olive_CQC_Shoulders_CO.paa",
+        "MA_Armor\data\Vests\MKVB\Shoulders\EVA\Olive_EVA_Shoulders_CO.paa",
+        "MA_Armor\data\Vests\MKVB\Shoulders\EVA\Olive_EVA_Shoulders_CO.paa",
+        "MA_Armor\data\Vests\MKVB\Shoulders\Grenadier\Olive_Grenadier_Shoulders_CO.paa",
+        "MA_Armor\data\Vests\MKVB\Shoulders\Grenadier\Olive_Grenadier_Shoulders_CO.paa",
+        "MA_Armor\data\Vests\MKVB\Shoulders\Gungnir\Olive_GUNGNIR_Shoulders_CO.paa",
+        "MA_Armor\data\Vests\MKVB\Shoulders\Gungnir\Olive_GUNGNIR_Shoulders_CO.paa",
+        "MA_Armor\data\Vests\MKVB\Shoulders\Hayabusa\Olive_Hayabusa_Shoulders_CO.paa",
+        "MA_Armor\data\Vests\MKVB\Shoulders\Hayabusa\Olive_Hayabusa_Shoulders_CO.paa",
+        "MA_Armor\data\Vests\MKVB\Shoulders\JFO\Olive_JFO_Shoulders_CO.paa",
+        "MA_Armor\data\Vests\MKVB\Shoulders\JFO\Olive_JFO_Shoulders_CO.paa",
+        "34thPRC_ArmourStandard\data\mkiv\34_MKIV_Shoulders.paa",
+        "34thPRC_ArmourStandard\data\mkiv\34_MKIV_Shoulders.paa",
+        "MA_Armor\data\Vests\MKVB\Shoulders\MKV\Olive_MKV_Shoulders_CO.paa",
+        "MA_Armor\data\Vests\MKVB\Shoulders\MKV\Olive_MKV_Shoulders_CO.paa",
+        "MA_Armor\data\Vests\MKVB\Shoulders\ODST\Olive_ODST_Shoulders_CO.paa",
+        "MA_Armor\data\Vests\MKVB\Shoulders\ODST\Olive_ODST_Shoulders_CO.paa",
+        "MA_Armor\data\Vests\MKVB\Shoulders\Operator\Olive_Operator_Shoulders_CO.paa",
+        "MA_Armor\data\Vests\MKVB\Shoulders\Operator\Olive_Operator_Shoulders_CO.paa",
+        "MA_Armor\data\Vests\MKVB\Shoulders\Recon\Olive_Recon_Shoulders_CO.paa",
+        "MA_Armor\data\Vests\MKVB\Shoulders\Recon\Olive_Recon_Shoulders_CO.paa",
+        "MA_Armor\data\Vests\MKVB\Shoulders\Security\Olive_Security_Shoulders_CO.paa",
+        "MA_Armor\data\Vests\MKVB\Shoulders\Security\Olive_Security_Shoulders_CO.paa",
+        "MA_Armor\data\Vests\MKVB\Shoulders\Scout\Olive_Scout_Shoulders_CO.paa"
+    };
+};
