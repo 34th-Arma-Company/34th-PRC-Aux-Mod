3b44085cfa8fb78022bc18e68e53cd8033856118
Snamberr <snake02422@gmail.com>
2024-11-19 17:51:08 -0500

    fixed config weapons

--- src/Addons/34thPRC_ArmourStandard/data/config_weapons.hpp
+++ src/Addons/34thPRC_ArmourStandard/data/config_weapons.hpp
@@ -3,5 +3,5 @@
 #include "mkiv\config_weapons.hpp"
 #include "mkvb\config_weapons.hpp"
 #include "odst\config_weapons.hpp"
-#include "spi\config_weapons.hpp"
 #include "odst2\config_weapons.hpp"
+#include "spi\config_weapons.hpp"
