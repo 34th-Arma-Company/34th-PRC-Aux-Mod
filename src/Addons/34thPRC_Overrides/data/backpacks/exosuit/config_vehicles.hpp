a3eca3902ecd944f88a674a96e73118ca26803fe
Lupus590 <lupussolitarius590@gmail.com>
2024-12-15 12:04:41 +0000

    spaces to tabs

--- src/Addons/34thPRC_Overrides/data/backpacks/exosuit/config_vehicles.hpp
+++ src/Addons/34thPRC_Overrides/data/backpacks/exosuit/config_vehicles.hpp
@@ -1,10 +1,10 @@
 class Bag_Base; // Vanilla?
 class Sinagami_Assault_Exo: Bag_Base // PhoenixSystems_Exosuits
 {
-    maximumLoad = 350;
+	maximumLoad = 350;
 }
 
 class Sinagami_Assault_Jetpack_Exo: Bag_Base // PhoenixSystems_Exosuits
 {
-    maximumLoad = 350;
+	maximumLoad = 350;
 }
