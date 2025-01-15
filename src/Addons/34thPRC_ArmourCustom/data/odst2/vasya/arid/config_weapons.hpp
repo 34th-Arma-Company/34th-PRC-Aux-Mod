e72a1a7d6dffe254b9b898f19ba3d42249aad245
OverYandere <126543615+OverYandere@users.noreply.github.com>
2025-01-15 15:48:58 -0500

    Vasya Custom Helm
    title

--- src/Addons/34thPRC_ArmourCustom/data/odst2/vasya/arid/config_weapons.hpp
+++ src/Addons/34thPRC_ArmourCustom/data/odst2/vasya/arid/config_weapons.hpp
@@ -15,3 +15,23 @@ class 34thPRC_ArmourCustom_ODST2_Vasya_CH252D_CustomVisor_Arid : 34thPRC_ArmourC
 		visor="custom";
 	};
 };
+class 34thPRC_ArmourCustom_ODST2_Vasya_CH252D_CustomVisor_Arid_dp : 34thPRC_ArmourCustom_ODST2_Shewolf_CH252D_CustomVisor_Urban_dp
+{
+	displayName="[34th] CH252D Arid 'Vasya' (Custom Visor)";
+	author="Over Yandere";
+	scope=0;
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_ArmourCustom\data\odst2\vasya\arid\vasya_odst_helmet_arid.paa",
+		"MA_Armor\data\Helmets\Human_ODST\Color_Variants\MA_ODST_Depolarized_Visor_CA.paa"
+	};
+	class ItemInfo:ItemInfo
+	{
+		uniformModel="MA_Armor\data\Helmets\Human_ODST\Reach_ODST_Helm_dp.p3d";
+		hiddenSelectionsTextures[]=
+		{
+			"MA_Armor\data\Helmets\Human_ODST\Color_Variants\MA_ODST_Helmet_CO.paa",
+			"MA_Armor\data\Helmets\Human_ODST\Color_Variants\MA_ODST_Depolarized_Visor_CA.paa"
+		};
+	};
+};
