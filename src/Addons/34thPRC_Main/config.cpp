45a2cf96c8ccfa3166b765080f97456620a0d5fe
RoutOfWolves <dianebbrown95@gmail.com>
2025-01-18 13:31:09 -0600

    Versioning file

---
+++ src/Addons/34thPRC_Main/config.cpp
@@ -0,0 +1,25 @@
+class CfgPatches
+{
+	class main
+	{
+		name = "main";
+		author = "34th PRC Modding Team";
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = "0.21.0"; //Update to match changelog version on release
+		requiredAddons[] =
+		{
+			"34thPRC_AceArsenalExtended",
+			"34thPRC_ArmourCustom",
+			"34thPRC_ArmourStandard",
+			"34thPRC_FactionBrimstone",
+			"34thPRC_LoadingScreens",
+			"34thPRC_Markers",
+			"34thPRC_Overrides",
+			"34thPRC_Vehicles",
+			"sc_uniforms",
+			"SEC_GuestPack",
+		};
+		version = "0.21.0"; //Update to match changelog version on release
+	};
+};
--- src/changelog.md
+++ src/changelog.md
@@ -11,6 +11,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - ACE Arsenal Extended Support added:
   - Misriah Armory Marine Pouches
   - OPTRE NVGs
+- Version checking
 
 ## 0.21.0
 ### Added
