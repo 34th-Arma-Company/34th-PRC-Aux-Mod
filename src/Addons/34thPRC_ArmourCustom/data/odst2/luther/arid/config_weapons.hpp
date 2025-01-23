8411d32a5b2a9028e21e15eb368e44afd54e141d
OverYandere <126543615+OverYandere@users.noreply.github.com>
2025-01-23 16:40:53 -0500

    Luther Custom Helmet (#346)
    * Luther Custom Helmet
    
    Did up the config for Luther's Custom helm with his supplied .paa files. They look okay but if I find the time I might redo the paa files for him.
    
    * Changelog Update

---
+++ src/Addons/34thPRC_ArmourCustom/data/odst2/luther/arid/config_weapons.hpp
@@ -0,0 +1,18 @@
+class 34thPRC_ArmourCustom_ODST2_Luther_JFO_Custom_Arid : 34thPRC_ArmourCustom_ODST2_Luther_JFO_Custom_Urban
+{
+	displayName="[34th] JFO Arid 'Luther'";
+	author="Over Yandere";
+	hiddenSelectionsTextures[]=
+	{
+		"34thPRC_ArmourCustom\data\odst2\luther\arid\jfo_helmet_arid.paa",
+		"34thPRC_ArmourStandard\data\mkvb\JFO\Visor\34_JFO_Helmet_Visor_Gold.paa"
+	};
+	class XtdGearInfo
+	{
+		model="34thPRC_ArmourCustom_Cappy_EOD_Helmet_Urban";
+		player="luther";
+		helmet="jfo";
+		camo="arid";
+		visor="default";
+	};
+};
