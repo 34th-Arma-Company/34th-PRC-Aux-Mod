1a75ed632e953b8efc77f860d61d8279402eb3ae
OverYandere <126543615+OverYandere@users.noreply.github.com>
2025-01-27 23:18:57 -0500

    Saving Work

---
+++ src/Addons/34thPRC_WeaponsStandard/SRSXM1AB/config.cpp
@@ -0,0 +1,25 @@
+class CfgPatches
+{
+	class SRSXM1AB
+	{
+		author="34th PRC Modding Team, Over Yandere";
+		addonRootClass="34thPRC_WeaponsStandard";
+		requiredAddons[]=
+		{
+			"34thPRC_WeaponsStandard",
+		};
+	};
+};
+class SRSXM1AB_Properties_Defualt
+{
+	fuse_distance_random_min=0;
+	fuse_distance_random_max=0;
+	fuse_distance_max=300;
+	fuse_distance_min=100;
+	fuse_explosion="OpTre_Plasma_Grenade_Explosion";
+};
+class CfgWeapons
+{
+	class OPTRE_LongRifle_Base;
+
+};
