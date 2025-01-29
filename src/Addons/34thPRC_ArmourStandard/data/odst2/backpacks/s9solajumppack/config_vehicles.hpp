aa3f286973179c265861443600c5f8d91a861782
Vazya <161765906+Vazya-H3lljumper@users.noreply.github.com>
2025-01-29 00:28:59 -0600

    The bot keeps yelling at me. There is no indentation other than what was copy pasted help.

--- src/Addons/34thPRC_ArmourStandard/data/odst2/backpacks/s9solajumppack/config_vehicles.hpp
+++ src/Addons/34thPRC_ArmourStandard/data/odst2/backpacks/s9solajumppack/config_vehicles.hpp
@@ -2,7 +2,7 @@ class OPTRE_S12_SOLA_Jetpack; // OPTRE ---> OPTRE_Weapons
 class SC_MercerJumppack; //Scion Conflict ---> sc_newequipment2
 class 34thPRC_ArmorStandard_Backpacks_ODST_S9SOLAJumppack : SC_MercerJumppack
 {
-    displayname="[34th] Series-9[B] SOLA Jump-Jet"
+	displayname="[34th] Series-9[B] SOLA Jump-Jet"
 	author="34th PRC - Vasya, Outworld Studios, & Article 2 Studios"
 	picture="\OPTRE_weapons\backpacks\icons\icon_jetpack_ca.paa";
 	scope=2;
