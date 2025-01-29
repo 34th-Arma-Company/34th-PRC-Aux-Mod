d1ea72ad20d7ec8b8001488d1a83e7d06bbcfe3f
Vazya <161765906+Vazya-H3lljumper@users.noreply.github.com>
2025-01-29 00:26:31 -0600

    Removing indentions becuase the bot keeps yelling at me.

--- src/Addons/34thPRC_ArmourStandard/data/odst2/backpacks/s9solajumppack/config_vehicles.hpp
+++ src/Addons/34thPRC_ArmourStandard/data/odst2/backpacks/s9solajumppack/config_vehicles.hpp
@@ -2,21 +2,21 @@ class OPTRE_S12_SOLA_Jetpack; // OPTRE ---> OPTRE_Weapons
 class SC_MercerJumppack; //Scion Conflict ---> sc_newequipment2
 class 34thPRC_ArmorStandard_Backpacks_ODST_S9SOLAJumppack : SC_MercerJumppack
 {
-    displayname			=	"[34th] Series-9[B] SOLA Jump-Jet"
-	author				=	"34th PRC - Vasya, Outworld Studios, & Article 2 Studios"
-	picture				=	"\OPTRE_weapons\backpacks\icons\icon_jetpack_ca.paa";
-	scope				=	2;
-	scopearsenal		=	2;
-	ace_gunbag			=	1;
-	tf_hasLRradio		=	1;
-	tf_range			=	25000;
-	tf_encryptionCode	=	"tf_west_radio_code";
-	tf_dialog			=	"rt1523g_radio_dialog";
-	tf_subtype			=	"digital_lr";
-	tf_dialogUpdate		=	"call TFAR_fnc_updateLRDialogToChannel;";
-	model				=	"OPTRE_Weapons\Backpacks\jetpack_on.p3d";
-	uniformModel		=	"OPTRE_Weapons\Backpacks\jetpack_on.p3d";
-	hiddenSelections[]	=
+    displayname="[34th] Series-9[B] SOLA Jump-Jet"
+	author="34th PRC - Vasya, Outworld Studios, & Article 2 Studios"
+	picture="\OPTRE_weapons\backpacks\icons\icon_jetpack_ca.paa";
+	scope=2;
+	scopearsenal=2;
+	ace_gunbag=1;
+	tf_hasLRradio=1;
+	tf_range=25000;
+	tf_encryptionCode="tf_west_radio_code";
+	tf_dialog="rt1523g_radio_dialog";
+	tf_subtype="digital_lr";
+	tf_dialogUpdate="call TFAR_fnc_updateLRDialogToChannel;";
+	model="OPTRE_Weapons\Backpacks\jetpack_on.p3d";
+	uniformModel="OPTRE_Weapons\Backpacks\jetpack_on.p3d";
+	hiddenSelections[]=
 		{
 			"camo"
 		};
