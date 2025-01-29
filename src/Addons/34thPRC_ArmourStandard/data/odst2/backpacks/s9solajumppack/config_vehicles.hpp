3f295d8c9a34f001470115671c964b3fd806b0bb
Vazya <161765906+Vazya-H3lljumper@users.noreply.github.com>
2025-01-28 22:25:07 -0600

    Jump-Jet is now funtional with a Halo model, and textures were implemented.

--- src/Addons/34thPRC_ArmourStandard/data/odst2/backpacks/s9solajumppack/config_vehicles.hpp
+++ src/Addons/34thPRC_ArmourStandard/data/odst2/backpacks/s9solajumppack/config_vehicles.hpp
@@ -1,11 +1,20 @@
 class OPTRE_S12_SOLA_Jetpack; // OPTRE ---> OPTRE_Weapons
-class SC_Mercer_Jumppack_Black; //Scion Conflict ---> sc_newequipment2
-class 34thPRC_ArmorStandard_Backpacks_ODST_S9SOLAJumppack : SC_Mercer_Jumppack_Black
+class SC_MercerJumppack; //Scion Conflict ---> sc_newequipment2
+class 34thPRC_ArmorStandard_Backpacks_ODST_S9SOLAJumppack : SC_MercerJumppack
 {
-    displayname  = "[34th] Series-9[B] SOLA Jump-Jet"
-	author       = "34th PRC - Vasya, Outworld Studios, & Article 2 Studios"
-	scope        = 2;
-	scopearsenal = 2;
-	model        = "OPTRE_Weapons\Backpacks\jetpack_on.p3d";
-	uniformModel = "OPTRE_Weapons\Backpacks\jetpack_on.p3d";
+    displayname        = "[34th] Series-9[B] SOLA Jump-Jet"
+	author             = "34th PRC - Vasya, Outworld Studios, & Article 2 Studios"
+	picture            = "\OPTRE_weapons\backpacks\icons\icon_jetpack_ca.paa";
+	scope              = 2;
+	scopearsenal       = 2;
+	model              = "OPTRE_Weapons\Backpacks\jetpack_on.p3d";
+	uniformModel       = "OPTRE_Weapons\Backpacks\jetpack_on.p3d";
+	hiddenSelections[] =
+		{
+			"camo"
+		};
+		hiddenSelectionsTextures[]=
+		{
+			"optre_weapons\backpacks\data\jetpack_co.paa"
+		};
 };
