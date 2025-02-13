6155a43d1d46e76a02683ef1f78e53afc1628163
Lupus590 <lupussolitarius590@gmail.com>
2025-02-13 23:27:38 +0000

    Forgot this bit

--- src/Addons/34thPRC_AceArsenalExtended/data/aax_models/weapons/armour_custom/odst2/group1/config.hpp
+++ src/Addons/34thPRC_AceArsenalExtended/data/aax_models/weapons/armour_custom/odst2/group1/config.hpp
@@ -179,7 +179,7 @@ class 34thPRC_ArmourCustom_ODST2_Yandere_CH252D_CustomVisor_Urban
 };
 
 //Custom ODST Specialist Helmets Helmets Group 1: Cappy, Yandere, Luther
-class 34thPRC_ArmourCustom_Cappy_EOD_Helmet_Urban : 34thPRC_ArmourStandard_EOD_Helmet_Urban
+class 34thPRC_ArmourCustom_Cappy_EOD_Helmet_Urban
 {
 	label="[34thPRC] Specialist Helmets Custom";
 	author="34th PRC Modding Team, Over Yandere";
@@ -201,6 +201,55 @@ class 34thPRC_ArmourCustom_Cappy_EOD_Helmet_Urban : 34thPRC_ArmourStandard_EOD_H
 		class luther
 		{
 			label="Luther";
+		};
+	};class helmet
+	{
+		alwaysSelectable=1;
+		changeingame=0;
+		label="Helmet Type";
+		values[]={"eod","gungnir","jfo","recon"};
+		class eod
+		{
+			label="EOD";
+		};
+		class gungnir
+		{
+			label="Gungnir";
+		};
+		class jfo
+		{
+			label="JFO";
+		};
+		class recon
+		{
+			label="Recon";
+		};
+	};
+	class camo
+	{
+		alwaysSelectable=1;
+		changeingame=0;
+		label="Camo Variant";
+		values[] = {"urban", "arctic", "arid", "tropic", "woodland"};
+		class urban
+		{
+			label="Urban";
+		};
+		class arctic
+		{
+			label="Arctic";
+		};
+		class arid
+		{
+			label="Arid";
+		};
+		class tropic
+		{
+			label="Tropic";
+		};
+		class woodland
+		{
+			label="Woodland";
 		};
 	};
 	class visor
