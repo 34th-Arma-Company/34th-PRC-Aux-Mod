3c8340c5ee2c9ae6771d08bbc8d34b81722c76eb
Lupus590 <lupussolitarius590@gmail.com>
2025-02-19 20:58:16 +0000

    more code style

--- src/Addons/34thPRC_Overrides/data/weapons/railgun/config_weapons.hpp
+++ src/Addons/34thPRC_Overrides/data/weapons/railgun/config_weapons.hpp
@@ -5,10 +5,10 @@ class OPTRE_FC_Railgun : OPTRE_Rifle_Base // OPTRE_Weapons --> /ar/cfgWeapons.hp
 	{
 		"34thPRC_Overrides_Weapons_Railgun_16x65mm_APSlug",
 		"34thPRC_Overrides_Weapons_Railgun_16x65mm_3RoundAPSlug",
-		"34thPRC_Overrides_Weapons_Railgun_16x65mm_HESlug"
+		"34thPRC_Overrides_Weapons_Railgun_16x65mm_HESlug",
 	};
 	magazineWell[]=
 	{
-		"OPTRE_FC_Railgun_Slug"
+		"OPTRE_FC_Railgun_Slug",
 	};
 };
