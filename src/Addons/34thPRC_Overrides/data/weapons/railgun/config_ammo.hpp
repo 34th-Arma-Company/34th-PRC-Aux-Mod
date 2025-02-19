c39489bd7b5fbb3d79ed62037aecbb987c6c4252
Vazya <161765906+Vazya-H3lljumper@users.noreply.github.com>
2025-02-19 10:53:39 -0600

    Code Style

--- src/Addons/34thPRC_Overrides/data/weapons/railgun/config_ammo.hpp
+++ src/Addons/34thPRC_Overrides/data/weapons/railgun/config_ammo.hpp
@@ -1,46 +1,45 @@
 class OPTRE_FC_16x65mm_Slug; // OPTRE_FC_Weapons --> /Ammo/cfgAmmo.hpp
-class 34thPRC_Overrides_Weapons_Railgun_AP_16x65mm: OPTRE_FC_16x65mm_Slug
+class 34thPRC_Overrides_Weapons_Railgun_AP_16x65mm : OPTRE_FC_16x65mm_Slug
+{
+	caliber=20;
+	hit=275;
+	indirectHit=20;
+	indirectHitRange=1;
+	explosive=0.3;
+	explosionEffects="ExploAmmoExplosion";
+	explosionEffectsDir="explosionDir";
+	ace_frag_enabled=1;
+	ace_frag_classes[]=
 	{
-		caliber=20;
-		hit=275;
-		indirectHit=20;
-		indirectHitRange=1;
-		explosive=0.3;
-		explosionEffects="ExploAmmoExplosion";
-		explosionEffectsDir="explosionDir";
-		ace_frag_enabled=1;
-		ace_frag_classes[]=
-		{
-			"ace_frag_large"
-		};
-		ace_frag_metal=1000;
-		ace_frag_charge=800;
-		ace_frag_gurney_c=2440;
-		ace_frag_gurney_k="1/2";
-		tracerStartTime=0.5;
+	"ace_frag_large"
 	};
-
+	ace_frag_metal=1000;
+	ace_frag_charge=800;
+	ace_frag_gurney_c=2440;
+	ace_frag_gurney_k="1/2";
+	tracerStartTime=0.5;
+};
 class 34thPRC_Overrides_Weapons_Railgun_HE_16x65mm: 34thPRC_Overrides_Weapons_Railgun_AP_16x65mm
+{
+	caliber=8;
+	hit=100;
+	indirectHit=75;
+	indirectHitRange=6;
+	explosive=1;
+	deflecting=4;
+	ACE_damageType="grenade";
+	ace_frag_enabled=1;
+	ace_frag_classes[]=
 	{
-		caliber=8;
-		hit=100;
-		indirectHit=75;
-		indirectHitRange=6;
-		explosive=1;
-		deflecting=4;
-		ACE_damageType="grenade";
-		ace_frag_enabled=1;
-		ace_frag_classes[]=
-		{
-			"ace_frag_large"
-		};
-		ace_frag_metal=1000;
-		ace_frag_charge=800;
-		ace_frag_gurney_c=2440;
-		ace_frag_gurney_k="1/2";
-		ace_rearm_caliber=39;
-		ace_vehicle_damage_incendiary=0.80000001;
-		explosionEffects="ExploAmmoExplosion";
-		craterEffects="ExploAmmoCrater";
-		CraterWaterEffects="ImpactEffectsWaterHE";
+	"ace_frag_large"
 	};
+	ace_frag_metal=1000;
+	ace_frag_charge=800;
+	ace_frag_gurney_c=2440;
+	ace_frag_gurney_k="1/2";
+	ace_rearm_caliber=39;
+	ace_vehicle_damage_incendiary=0.80000001;
+	explosionEffects="ExploAmmoExplosion";
+	craterEffects="ExploAmmoCrater";
+	CraterWaterEffects="ImpactEffectsWaterHE";
+};
--- src/Addons/34thPRC_Overrides/data/weapons/railgun/config_magazines.hpp
+++ src/Addons/34thPRC_Overrides/data/weapons/railgun/config_magazines.hpp
@@ -1,31 +1,31 @@
 class OPTRE_FC_Railgun_Slug; // OPTRE_FC_Weapons --> /Ammo/cfgMagazines.hpp
-class 34thPRC_Overrides_Weapons_Railgun_16x65mm_APSlug: OPTRE_FC_Railgun_Slug
-	{
-		author="Vasya"
-		ammo="34thPRC_Overrides_Weapons_Railgun_AP_16x65mm";
-		displayName="1Rnd 16x65mm AP Slug";
-		displayNameShort="1rnd 16x65mm Slug";
-		descriptionShort="16x65mm Slug with penetrator tip for anti-vehicle capability";
-		count=1;
-		mass=10;
-		tracersEvery=1;
-	};
+class 34thPRC_Overrides_Weapons_Railgun_16x65mm_APSlug : OPTRE_FC_Railgun_Slug
+{
+	author="Vasya"
+	ammo="34thPRC_Overrides_Weapons_Railgun_AP_16x65mm";
+	displayName="1Rnd 16x65mm AP Slug";
+	displayNameShort="1rnd 16x65mm Slug";
+	descriptionShort="16x65mm Slug with penetrator tip for anti-vehicle capability";
+	count=1;
+	mass=10;
+	tracersEvery=1;
+};
 class 34thPRC_Overrides_Weapons_Railgun_16x65mm_3RoundAPSlug: 34thPRC_Overrides_Weapons_Railgun_16x65mm_APSlug
-	{
-		author="Vasya"
-		displayName="3Rnd 16x65mm AP Slug";
-		displayNameShort="3rnd 16x65mm Slug";
-		descriptionShort="Three 16x65mm Slugs with penetrator tips for rapid fire, anti-vehicle capability";
-		count=3;
-		mass=30;
-	};
+{
+	author="Vasya"
+	displayName="3Rnd 16x65mm AP Slug";
+	displayNameShort="3rnd 16x65mm Slug";
+	descriptionShort="Three 16x65mm Slugs with penetrator tips for rapid fire, anti-vehicle capability";
+	count=3;
+	mass=30;
+};
 class 34thPRC_Overrides_Weapons_Railgun_16x65mm_HESlug: 34thPRC_Overrides_Weapons_Railgun_16x65mm_APSlug
-	{
-		author="Vasya"
-		ammo="34thPRC_Overrides_Weapons_Railgun_HE_16x65mm";
-		displayName="1Rnd 16x65mm HE Slug";
-		displayNameShort="1rnd 16x65mm HE Slug";
-		descriptionShort="16x65mm slug packed with High Explosive filler for anti-personnel capability";
-		count=1;
-		mass=20;
-	};
+{
+	author="Vasya"
+	ammo="34thPRC_Overrides_Weapons_Railgun_HE_16x65mm";
+	displayName="1Rnd 16x65mm HE Slug";
+	displayNameShort="1rnd 16x65mm HE Slug";
+	descriptionShort="16x65mm slug packed with High Explosive filler for anti-personnel capability";
+	count=1;
+	mass=20;
+};
