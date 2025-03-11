ed049894f04f228e113b1a3dc1c3a926fca31acb
Lupus590 <lupussolitarius590@gmail.com>
2025-03-11 21:46:46 +0000

    hope this works

--- prepRelease.ps1
+++ prepRelease.ps1
@@ -1,13 +1,19 @@
-# get user to edit changelog
-Write-Host "Please edit the changelog.md file and save your changes."
-if ($IsWindows -or [System.Environment]::OSVersion.Platform -eq "Win32NT") {
-    Start-Process "changelog.md"
-} elseif ($IsLinux -or $IsMacOS -or [System.Environment]::OSVersion.Platform -eq "Unix") {
-    Start-Process "xdg-open" "changelog.md"
-} else {
-    Write-Host "Unsupported OS. Please manually open changelog.md in your preferred text editor."
+param (
+    [switch]$CI
+)
+
+if (-not $CI) {
+    # get user to edit changelog
+    Write-Host "Please edit the changelog.md file and save your changes."
+    if ($IsWindows -or [System.Environment]::OSVersion.Platform -eq "Win32NT") {
+        Start-Process "changelog.md"
+    } elseif ($IsLinux -or $IsMacOS -or [System.Environment]::OSVersion.Platform -eq "Unix") {
+        Start-Process "xdg-open" "changelog.md"
+    } else {
+        Write-Host "Unsupported OS. Please manually open changelog.md in your preferred text editor."
+    }
+    Read-Host "Press Enter to continue after saving the changelog."
 }
-Read-Host "Press Enter to continue after saving the changelog."
 
 # extract new version number from changelog.md
 $changelogContent = Get-Content "changelog.md"
