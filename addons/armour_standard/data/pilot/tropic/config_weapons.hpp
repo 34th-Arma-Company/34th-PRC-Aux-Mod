500cac191597e35e4ce3d39457dee55904019f4a
OverYandere <126543615+OverYandere@users.noreply.github.com>
2025-03-28 16:24:38 -0400

    fixes
    fixed a number of path issues

--- addons/armour_standard/data/pilot/tropic/config_weapons.hpp
+++ addons/armour_standard/data/pilot/tropic/config_weapons.hpp
@@ -5,7 +5,7 @@ class 34thPRC_ArmouStandard_Pilot_IHADSS_Helmet_Tropic : 34thPRC_ArmouStandard_P
 	hiddenSelectionsTextures[]=
 	{
 		"\z\34thprc\addons\armour_standard\data\pilot\pilot_helmet_addons.paa",
-		"\z\34thprc\addons\armour_standard\pilot\tropic\pilot_helmet_ext.paa",
+		"\z\34thprc\addons\armour_standard\data\pilot\tropic\pilot_helmet_ext.paa",
 		"DMNS\DMNS_Units\Armour\Pilot_Helm_01\Data\H2A_Pilot_Helm_INT_co.paa",
 		"DMNS\DMNS_Units\Armour\Pilot_Helm_01\Data\H2A_Pilot_Helm_MID_co.paa",
 		"\z\34thprc\addons\armour_standard\data\pilot\pilot_helmet_visor.paa"
