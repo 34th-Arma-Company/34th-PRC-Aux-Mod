c7b0669bcedfad87d8396b57b705b868d57170af
RoutOfWolves <dianebbrown95@gmail.com>
2025-04-03 17:56:57 -0500

    AAX for the crewman helmets (#522)
    closes #490

---
+++ addons/ace_arsenal_extended/data/aax_models/weapons/armour_standard/crewman/config.hpp
@@ -0,0 +1,37 @@
+class 34thPRC_ArmourStandard_Marines2_CH252C_Urban
+{
+	label = "[34th] CH252C Helmets";
+	author = "34th PRC Modding Team, Moz";
+	options[] = {"camo"};
+	class camo
+	{
+		alwaysSelectable = 1;
+		changeingame = 0;
+		values[] = {"urban", "arctic", "arid", "tropic", "woodland"};
+		class urban
+		{
+			label = "Urban";
+			image = "#(rgb,8,8,3)color(0.071,0.071,0.071,1)";
+		};
+		class arctic
+		{
+			label = "Arctic";
+			image = "#(rgb,8,8,3)color(0.322,0.322,0.322,1)";
+		};
+		class arid
+		{
+			label = "Arid";
+			image = "#(rgb,8,8,3)color(0.282,0.267,0.239,1)";
+		};
+		class tropic
+		{
+			label = "Tropic";
+			image = "#(rgb,8,8,3)color(0.196,0.2,0.122,1)";
+		};
+		class woodland
+		{
+			label = "Woodland";
+			image = "#(rgb,8,8,3)color(0.149,0.153,0.125,1)";
+		};
+	};
+};
